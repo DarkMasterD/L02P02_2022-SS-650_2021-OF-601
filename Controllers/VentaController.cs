68f4e8e616d7342dc7b2d1fd58d685984fb6cd35
Juan Orellana <juan.orellana2@catolica.edu.sv>
2025-04-01 19:42:16 -0600

    Comprobar funcionalidad de CarritoVenta y metodo de VentaInicio

--- Controllers/VentaController.cs
+++ Controllers/VentaController.cs
@@ -1,5 +1,6 @@
 ﻿using L02P02_2022_SS_650_2021_OF_601.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L02P02_2022_SS_650_2021_OF_601.Controllers
 {
@@ -10,12 +11,8 @@ namespace L02P02_2022_SS_650_2021_OF_601.Controllers
         {
             _libreriaContext = context;
         }
-        public IActionResult InicioVenta()
-        {
-            return View();
-        }
         [HttpGet]
-        public IActionResult CarritoVenta()
+        public IActionResult CarritoVenta(int id_pedido)
         {
             var libros = (from l in _libreriaContext.libros 
                           join a in _libreriaContext.autores
@@ -28,22 +25,56 @@ namespace L02P02_2022_SS_650_2021_OF_601.Controllers
                               precio = l.precio,
                           }).ToList();
             ViewData["listadoLibros"] = libros;
+            ViewBag.IdPedido = id_pedido;
             return View();
         }
         [HttpPost]
-        public IActionResult CarritoVenta(int id_libro)
+        public IActionResult CarritoVenta(int id_libro, int id_pedido)
         {
             pedido_detalle pedido = new pedido_detalle();
-            pedido.id_pedido = 1;
+            pedido.id_pedido = id_pedido;
             pedido.id_libro = id_libro;
             pedido.created_at = DateTime.Now;
 
             _libreriaContext.pedido_detalle.Add(pedido);
             _libreriaContext.SaveChanges();
 
+            pedido_encabezado? ActPedido = (from p in _libreriaContext.pedido_encabezado
+                                                 where p.id == id_pedido
+                                                 select p).FirstOrDefault();
 
+            ActPedido.cantidad_libros = ActPedido.cantidad_libros + 1;
+
+            _libreriaContext.Entry(ActPedido).State = EntityState.Modified;
+            _libreriaContext.SaveChanges();
 
             return Json(new { success = true });
+        }
+        [HttpGet]
+        public IActionResult InicioVenta(int id_pedido, int id_cliente)
+        {
+            clientes? Cliente = (from c in _libreriaContext.clientes
+                                      where c.id == id_cliente
+                                      select c).FirstOrDefault();
+
+            var DetPedido = (from pd in _libreriaContext.pedido_detalle
+                             join l in _libreriaContext.libros
+                             on pd.id_libro equals l.id
+                             join a in _libreriaContext.autores
+                             on l.id_autor equals a.id 
+                             where pd.id == id_pedido
+                             select new
+                             {
+                                 libro = l.nombre,
+                                 autor = a.autor,
+                                 precio = l.precio
+                             }).ToList();
+
+            ViewData["listadoDetPedido"] = DetPedido;
+            ViewBag.cliente = Cliente;
+            ViewBag.id_pedido = id_pedido;
+
+            return View();
         }
     }
 }
